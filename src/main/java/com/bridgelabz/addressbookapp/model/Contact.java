cca10fb552e9e770aaf1de060b9fbd16d2358f6c
srijan0412 <srijan.soni01@gmail.com>
2025-03-06 10:50:21 +0530

    [Srijan] Add . Adding Model and DTO to the application

---
+++ src/main/java/com/bridgelabz/addressbookapp/model/Contact.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.addressbookapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Contact(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+//    Getter and setter
+}
