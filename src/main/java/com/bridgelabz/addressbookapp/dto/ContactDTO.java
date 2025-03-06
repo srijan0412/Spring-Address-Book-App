afde2456e9bead52f45b8efb72cb88f0a3240581
srijan0412 <srijan.soni01@gmail.com>
2025-03-06 13:07:11 +0530

    [Srijan] Add . Adding Lombok to autogenerate getter and setter for the ContactDTO

--- src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
@@ -1,35 +1,15 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDTO {
     private String name;
     private String email;
     private String phone;
-
-    public ContactDTO() {}
-
-    public ContactDTO(String name, String email, String phone) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    // Getter and Setter
-    public String getEmail() {
-        return email;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
